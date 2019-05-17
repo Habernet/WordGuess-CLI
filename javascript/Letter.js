3e9eb705b004b03f4f361d3ab87fb74bc71d2dba
habernet <phaberern@gmail.com>
2019-05-17 15:11:19 -0400

    Letter.js skeleton updated

--- javascript/Letter.js
+++ javascript/Letter.js
@@ -7,3 +7,18 @@
 //   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 
 //   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+exports.Letter = function () {
+    // a string value to stoer the underlying character for the letter
+    this.underlying = '';
+    // a boolean value that stores whether underlying has been guessed yet
+    this.guessed = false;
+    this.response = function () {
+        // a function that returns underlying if guessed is true...or an underscore if guessed is false
+
+    };
+    this.check = function (character) {
+        // takes a character as an arg and checks against underlying...update guessed accordingly
+
+    };
+};
