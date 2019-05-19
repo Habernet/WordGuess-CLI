56fe5429e6a0b8c618d33af8eaa9aee706389351
habernet <phaberern@gmail.com>
2019-05-19 14:10:51 -0400

    Letter.js written, not tested

--- javascript/Letter.js
+++ javascript/Letter.js
@@ -1,24 +1,27 @@
-// * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
-
-//   * A string value to store the underlying character for the letter
-
-//   * A boolean value that stores whether that letter has been guessed yet
-
-//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-
-//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-
-exports.Letter = function () {
-    // a string value to stoer the underlying character for the letter
-    this.underlying = '';
+exports.Letter = function (character) {
+    // a string value to store the underlying character for the letter
+    this.character = character;
     // a boolean value that stores whether underlying has been guessed yet
     this.guessed = false;
-    this.response = function () {
+    this.display = function () {
         // a function that returns underlying if guessed is true...or an underscore if guessed is false
 
-    };
-    this.check = function (character) {
-        // takes a character as an arg and checks against underlying...update guessed accordingly
+        if (this.character === ' ') {
+            // return space if word has space
+            return ' ';
+        } else if (!this.guessed){
+            // return underscore if it was not guessed correctly
+            return '_';
+        }else {
+            // return the underlying character because it was guessed correctly
+            return this.character;
+        };
 
+    };
+    this.check = function (guess) {
+        if (guess === this.character){
+            this.guessed = true;
+        }
+        // takes a character as an arg and checks against underlying... if it is equal to underlying..update guessed accordingly
     };
 };
