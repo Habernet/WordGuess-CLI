aa14e5ce8d66a7fae78f01a7f2aa1ab9ed7417b9
habernet <phaberern@gmail.com>
2019-05-20 19:09:08 -0400

    Letter and Word both written and tested. Working as expected.

--- javascript/Word.js
+++ javascript/Word.js
@@ -1,9 +1,4 @@
-
-// * **Word.js**: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. That means the constructor should define:
-
-
 var Letter = require('./Letter');
-
 
 //   * A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in `Letter.js`)
 function Word(word){
@@ -11,24 +6,17 @@ function Word(word){
     this.word = word;
     //array of 'new' Letter objects representing the letters of the underlying word
     this.wordArray= [];
-    // Fill the array based on the word
+    // Function to fill the array based on the word
     this.createWord = function() {
         for (let i = 0; i < word.length; i++) {
             this.wordArray.push(new Letter(this.word[i]));
         };
     };
-    this.test = function () {
-        console.log(this.wordArray);
-    };
     //   * A function that returns a string representing the word. This should call the function on each letter object (the first function defined in `Letter.js`) that displays the character or an underscore and concatenate those together.
     this.displayWord = function(){
-        for (let i =0; i < wordArray.length; i++) {
-
-        }
+        for (let i =0; i < this.wordArray.length; i++) {
+            return this.wordArray.join(' '); //JS calls toString automatically on each char object and then joins them together
+        };
     };
 };
-
-var test = new Word('Hello');
-test.createWord();
-test.test();
 module.exports = Word;
